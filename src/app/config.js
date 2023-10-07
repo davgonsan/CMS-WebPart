45905ed435951eab45a34434316ef715d37b5677
David Gonzalez <david888mm@gmail.com>
2023-10-07 14:33:34 -0400

    changes

--- src/app/config.js
+++ src/app/config.js
@@ -1,3 +1,4 @@
 export const {
-     API_URL
-     } = process.env
+     API_URL,
+     STRAPI_URL
+} = process.env
--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,37 @@
+import Link from "next/link"
 import { API_URL } from "./config"
-async function getGames(){
-  const res = await fetch (`${API_URL}/http://localhost:1337/api/video-games?populate=[platforms][fields][0]=name&populate[cover][fields][0]=url`)
-}
-if(!res.ok){
-  throw new Error ('Something went wrong')
-}
-const { data } = (await res).json()
-return data
+import { getCoverImage, getGames } from "./services/videosgames"
+import parse from "./snarkdown";
+import { Pagination } from "./components/Pagination";
+
 
 export default async function Home() {
-  const games = await getGames()
-  console.log(games)
+  const {data: games, pagination} = await getGames({page: 1})
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
+      
       {
-        games.map(game =>(
-        {game}
+        games.map(({attributes, id}) => (
+        
+        <Link key = {attributes.id} href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img 
+                className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" 
+                src={getCoverImage({ attributes })} alt="" />
+
+                <div className="flex flex-col justify-between p-4 leading-normal">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        {attributes.title}
+                    </h5>
+                        <p
+                        className="mb-3 font-normal text-gray-700 dark:text-gray-400"
+                        dangerouslySetInnerHTML={{ __html: parse (attributes.description)}} />
+                </div>
+        </Link>
+
         ))
       }
-      
+    <Pagination pagination={pagination} />
     </main>
   )
 }

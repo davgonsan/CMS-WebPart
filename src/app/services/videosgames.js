1ff33d0ae13dabd610c0f7ae5eedfa2341d6a784
David Gonzalez <david888mm@gmail.com>
2023-10-08 21:07:47 -0400

    page_function

--- src/app/services/videosgames.js
+++ src/app/services/videosgames.js
@@ -1,8 +1,9 @@
 import { API_URL, STRAPI_URL } from "../config"
 
-export async function getGames({page = 1}){
+export async function getGames({page = +page}){
   
     const res = await fetch (`${API_URL}/video-games?populate=[platforms][fields][0]=name&populate[cover][fields][0]=url&pagination[page]=${page}&pagination[pageSize]=1`)
+    /*http://localhost:1337/api/video-games?populate=[platforms][fields][0]=name&populate[cover][fields][0]=url&pagination[page]=${page}&pagination[pageSize]=1*/
   if(!res.ok){
     throw new Error ('Something went wrong')
   }
